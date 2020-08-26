0ed3d679ba826e7b0ae22d4e434a81f86ed7230c
Peergrammer <phdwight@gmail.com>
2020-08-26 15:17:23 +0800

    CVP-1: Provide the signup form.

--- account/views.py
+++ account/views.py
@@ -1,43 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm, EducationEditForm
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Education
+import logging
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cleaned_data = form.cleaned_data
-            user = authenticate(request, 
-                            username=cleaned_data['username'],
-                            password=cleaned_data['password'])
-            if user is not None:
-                login(request, user)
-                return HttpResponse('Successfully logged')
-            else:
-                return HttpResponse('Invalid Login')
-    else:
-        form = LoginForm()
-    
-    return render(request, 'account/login.html', {'form': form})
+logger = logging.getLogger(__name__)
 
 @login_required
 def panel(request):
     return render(request, 'account/panel.html', {'section': 'panel'})
 
-
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
+        logger.info("@register POST -- after the user submmitted the create account")
         if user_form.is_valid():
             new_user = user_form.save(commit=False) #dont commit yet
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             # Create the profile associated with this registration
             Profile.objects.create(user=new_user)
+            #Education.objects.create(user=new_user)
+            # Send an email to the user
+            send_email_to_new_user(user_form.cleaned_data)
+            sent = True
             return render(request, 'account/register_done.html', {'new_user': new_user})
+        else:
+            logger.debug("DEBUG something error in POST")
+            logger.error("something error in POST")
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
@@ -48,12 +41,22 @@ def edit(request):
         user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(instance=request.user.profile,
                                         data=request.POST)
+        education_form = EducationEditForm(instance=request.user.profile,
+                                        data=request.POST)
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            education_form.save()
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-    
+        education_form = EducationEditForm(instance=request.user.profile)
     return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+
+
+def send_email_to_new_user(cleaned_data):
+    subject = f"Welcome {cleaned_data['first_name']} {cleaned_data['last_name']}to CV Portal"
+    message = f"You are now a registered user. Your username is {cleaned_data['username']}"
+    send_mail(subject, message, 'admin@cvportal.com',[cleaned_data['email']])
+    logger.info("email sent")
